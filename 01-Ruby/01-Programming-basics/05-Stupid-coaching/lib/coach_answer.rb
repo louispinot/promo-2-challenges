1b7793c31d571c60d96845e53c4f49c63bad5e40
Louis Pinot <lpvillechenon@gmail.com>
2014-04-08 20:54:35 +0200

    halfway there

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,8 +1,17 @@
 def coach_answer(your_message)
-  # TODO: return coach answer to your_message
+  return if your_message == "I am going to work right now SIR !"
+  if your_message[-1..-1] == "?"
+    "Silly question! get dressed and go to work !"
+  else
+    "I don't care son, get dressed and go to work !"
+  end
+
 end
 
 def coach_answer_enhanced(your_message)
   # TODO: return coach answer to your_message, with additional custom rules of yours !
+  if your_message == your_message.upcase
+    "I can feel your motivation son ! " + coach_answer(your_message)
+  else coach_answer(your_message)
+  end
 end
-
--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,3 +1,9 @@
 require_relative "coach_answer"
 #TODO: Implement the program that makes you discuss with your coach from the terminal.
+puts "Your coach is standing in front of you, you feel like talking to him!"
+speech = gets.chomp
 
+until coach_answer_enhanced(speech)==nil do
+  puts coach_answer_enhanced(speech)
+  speech = gets.chomp
+end
