40117d06175a2a9e8ce7ce007a37f92cd6a5980d
Louis Pinot <lpvillechenon@gmail.com>
2014-04-08 21:21:36 +0200

    first round of cleaning

--- 01-Ruby/01-Programming-basics/06-Code-style-Gym/lib/codestyle_gym.rb
+++ 01-Ruby/01-Programming-basics/06-Code-style-Gym/lib/codestyle_gym.rb
@@ -1,44 +1,45 @@
-class Player_moving
-  Grid_POSITIONS = (1..100).to_a
-  
-def initialize ( position ) 
-  @position = position ; @iterations = 0; 
-raise "Initial position must be between 1 and 100" unless Grid_POSITIONS.include? @position
-  end
-  
-      def move   distanceInMeters
-@position += distanceInMeters
-  end
-  
-def play
-  if(  self.status_Player==:play   ) 
-if rand(1..100) < 50 ; move(rand(1..10))
-  else
-        move(-rand(1..10))
-end
-  end
-end
+class PlayerMoving
+  grid_positions = (1..100).to_a
 
-def cheat; @position = 101; end
-  
-def status_Player
-    if( @position >   100 )
-      :win 
-    elsif(@position<0) ; :loose
+  def initialize(position)
+    @position = position ; @iterations = 0;
+    raise "Initial position must be between 1 and 100" unless grid_positions.include? @position
+  end
+
+  def move(distance_in_meters)
+    @position += distance_in_meters
+  end
+
+  def status_player
+    if @position >= 100 ; :win
+    elsif @position <= 0 ; :loose
     else;  :play
     end
   end
-  
-  def hasWon ; status_Player  == :win ; end
-    
+
+  def play
+    if self.status_player == :play
+      if rand(1..100) < 50
+        move(rand(1..10))
+      else
+        move(-rand(1..10))
+      end
+    end
+  end
+
+  def cheat
+    @position = 101; end
+
+  def has_won ; status_Player  == :win ; end
+
 end
 
-player = Player_moving.new(50)
+player = PlayerMoving.new(50)
 player.play
-puts player.status_Player == :play # true
+puts player.status_player == :play # true
 player.cheat
-puts player.hasWon == true
-  
+puts player.has_won == true
+
 begin
   Player_moving.new(110)
 rescue RuntimeError => e
