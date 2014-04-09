c6afb7f09cb84245135ec87e7a705d92c0537dda
Louis Pinot <lpvillechenon@gmail.com>
2014-04-09 16:55:57 +0200

    Vive le PMU sa mère!

--- 01-Ruby/02-Arrays-Enumerable/06-Destructive-methods/lib/pmu.rb
+++ 01-Ruby/02-Arrays-Enumerable/06-Destructive-methods/lib/pmu.rb
@@ -1,7 +1,12 @@
 def pmu_format!(race_array)
   #TODO: modify the given array so that it is PMU-consistent
+  race_array.shuffle!
+    for i in 0..(race_array.count-1)
+      race_array[i] = "#{(i+1).to_s} - #{race_array[i]}!"
+    end
+  race_array
 end
 
-
+puts pmu_format!(["Abricot du Laudot", "Black Caviar", "Brigadier Gerard"])
 
 
