02f9aff63e2cfe3d64869579a2a18011528eaa5f
Louis Pinot <lpvillechenon@gmail.com>
2014-04-09 14:53:39 +0200

    Finally!!

--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
@@ -11,11 +11,21 @@ begin
 
   students.push(name)
 
-end while name != ""
+end until name == ""
 
+students = students[0..-2]
+student_count = students.count
+list_of_students = ""
 # TODO: Call `wagon_sort` method and display the sorted student list
 
 wagon_sort(students)
-puts "Congratulations! Your Wagon has #{students.count - 1} students:"
-for student in students do
-  print "#{student}, " end
+
+for i in 0..student_count-2 do
+    list_of_students << students[i].to_s
+    list_of_students << ", "
+end
+
+list_of_students = list_of_students[0..-3].to_s + " and " + students[student_count-1].to_s
+
+puts "Congratulations! Your Wagon has #{students.count} students:
+        #{list_of_students}"
