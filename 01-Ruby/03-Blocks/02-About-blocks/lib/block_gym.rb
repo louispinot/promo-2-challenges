7dd824cfddf06bcd43777c7cf47f2476bd702614
Louis Pinot <lpvillechenon@gmail.com>
2014-04-10 13:53:52 +0200

    tag method et timer_for

--- 01-Ruby/03-Blocks/02-About-blocks/lib/block_gym.rb
+++ 01-Ruby/03-Blocks/02-About-blocks/lib/block_gym.rb
@@ -1,10 +1,23 @@
 def tag(tag_name, attr = nil)
   #TODO:  Build HTML tags around  content given in the block
-  #       The method can be called with an optional HTML attribute inputted as [attr_name, attr_value]
+  #       The meth  od can be called with an optional HTML attribute inputted as [attr_name, attr_value]
+  my_tag = "<#{tag_name}"
+  my_tag += " #{attr[0]}='#{attr[1]}'" unless attr == nil
+  my_tag += ">"
+
+  my_tag += yield
+
+  my_tag += "</#{tag_name}>"
+
+  my_tag
 end
 
 def timer_for
   #TODO:  Return time taken to execute the given block
+  start = Time.now
+  yield
+  finish = Time.now
+  time = finish - start
 end
 
 def transform(element)
