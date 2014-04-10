aa50c08bb76245134fb4a4ee6b945c317da9e32b
Louis Pinot <lpvillechenon@gmail.com>
2014-04-10 10:08:26 +0200

    BIM!! Old numeral DONE!

--- 01-Ruby/02-Arrays-Enumerable/Optional-01-Roman-numerals/lib/roman_numerals.rb
+++ 01-Ruby/02-Arrays-Enumerable/Optional-01-Roman-numerals/lib/roman_numerals.rb
@@ -1,75 +1,48 @@
 def old_roman_numeral(an_integer)
+  arab_numbers = an_integer.to_s.split("").reverse.map {|digit| digit.to_i}
+  coordinates = mapping_coordinates(arab_numbers)
+  map = [["V", "I"],["L", "X"], ["D", "C"]]
 
-  #digit_inf_a_5 = ["I", "X", "C"]
-  #digit_sup_a_5 = ["V", "L", "D"]
+  roman_numerals = conversion(map,coordinates)
+
+  roman_numerals = roman_numerals.reverse.join
+
+  print roman_numerals
+  puts
+
+end
 
 
-
-  numeral = an_integer.to_s.split("").reverse.map {|digit| digit.to_i}
-
+def mapping_coordinates(arab_numbers)
   translator = Hash.new
-
-  numeral.each {|digit|
+  arab_numbers.each {|digit|
     if digit < 5
       translator[digit] = [0, digit]
     else
       translator[digit] = [1, (digit-5)]
     end  }
-
-  print translator
-  puts
-
-  map = [["v", "i"],["l", "x"], ["d", "c"]]
-
   coordinates=[]
-
-  numeral.each {|digit|
+  arab_numbers.each {|digit|
     coordinates << translator[digit]}
-
-  print coordinates
-  puts
-
-  roman = []
-####### ecrire une fonction ayant 2 array pr input qui ressort un array.
-  for k in 0..(coordinates.count-1) do
-    roman[k] = coordinates[k][0]*map[k][0] + coordinates[k][1]*map[k][1]
-  end
-
-print roman
-puts
-
-#  translator.each_key {|key|
- #   if translator[key]=O
-  #    key = key.to_i.
-   # }
-
-
-
-=begin
-  roman = []
-  translator = translator.each {|arab, roman|
-    if arab.to_i < 5
-      roman = 0
-    else roman = 1
-    end }
-
-    puts translator
-
-
-  for i in 0..(numeral.count) do
-
-    if numeral[i].to_i < 5
-      roman[i] = ref[i][0]*numeral[i].to_i
-    else roman[i] = ref[i][1] + ref[i][0]*numeral[i].to_i
-    end
-
-  end
-=end
+  coordinates
 end
+
+
+def conversion(map,coordinates)
+  converted_array = []
+  for i in 0..(coordinates.count-1) do
+    gps = []
+    coordinates[i][0].times {gps.push(map[i][0])}
+    coordinates[i][1].times {gps.push(map[i][1])}
+    converted_array << gps.join
+  end
+  converted_array
+end
+
 
 
 def roman_numeral(an_integer)
   #TODO: translate integer in roman number - modern-style way
 end
 
-print old_roman_numeral(549)
+print old_roman_numeral(40)
