2b3cfc573fc11531ec860fae328455d9d443de51
Louis Pinot <lpvillechenon@gmail.com>
2014-04-10 17:34:46 +0200

    my code is DRYer

--- 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
+++ 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
@@ -1,23 +1,18 @@
 # Encoding: utf-8
 
 def louchebem_translator(word, vowels=%w(a e i o u y))
-
-    do ### pour les mots de plus d'une lettre
-
-    if vowels.include? word[0]  # commence par une voyelle
-      word = word.insert(0, "l")
-
-
-    else   #commence par une consonne
-
-
-    end   ## fin du "if begins by vowels"
-    end unless word.length == 1
-
+    unless word.length < 3
+      prepare_end(word)
+      deal_with_start(word)
+      pimp_the_end(word)
+    end
 end
 
 
 def prepare_end (word, vowels=%w(a e i o u y))
+    # last_character = word[-1]
+    # penultimate_character = word[-2]
+
     unless vowels.include? word[-1]
       word = word[0..-2] if vowels.include? word[-2]
       word[-2..-1] = "é" if word[-2..-1] == "er"
@@ -27,10 +22,11 @@ end
 
 def deal_with_start (word, vowels=%w(a e i o u y))
     unless vowels.include? word[0]  # commence par une voyelle
-      if vowels.include? word[1]  # 2e lettre est une consonne
-        start = word[0..1]
-        word = word[2..-1] + start
-      else                        # 2e lettre est une voyelle
+      move_2_letters = {word[0..1] = "qu" || word[0..1] = "gu" || vowels.include? word[1]}
+      if move_2_letters
+          start = word[0..1]
+          word = word[2..-1] + start
+      else
         start = word[0]
         word = word[1..-1] + start
       end
@@ -38,9 +34,11 @@ def deal_with_start (word, vowels=%w(a e i o u y))
 end
 
 
-def pimp_the_end (word())
-
-
+def pimp_the_end (word)
+    ending_options = ["em", "é", "ji", "oc", "ic", "uche", "ès"]
+    ending = ending_options.sample
+    word = word.insert(-1, ending)
+end
 
 
 
