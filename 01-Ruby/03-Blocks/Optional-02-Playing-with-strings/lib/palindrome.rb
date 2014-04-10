dbb3a47c00eeae7b712252c3447aa07f07b3c7af
Louis Pinot <lpvillechenon@gmail.com>
2014-04-10 21:48:29 +0200

    pallindroming like a boss

--- 01-Ruby/03-Blocks/Optional-02-Playing-with-strings/lib/palindrome.rb
+++ 01-Ruby/03-Blocks/Optional-02-Playing-with-strings/lib/palindrome.rb
@@ -1,8 +1,9 @@
 def palindrome?(a_string)
   #TODO: check if a_string is a palindrome
+  a_string.downcase!.gsub!(/\W/, "") == a_string.reverse
 end
 
-  
+
 # Testing palindrome? function
 puts "#{palindrome?("A man, a plan, a canal -- Panama")}" #=> true
 puts "#{palindrome?("Madam, I'm Adam!")}" # => true
