52e9a2c2fda5d31f3a30a6ce745a6e67f4ec0b11
Louis Pinot <lpvillechenon@gmail.com>
2014-04-11 16:27:37 +0200

    i am a true roman

--- 01-Ruby/04-Hash-Symbols/Optional-01-Roman-to-integer/lib/roman_to_integer.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-01-Roman-to-integer/lib/roman_to_integer.rb
@@ -1,9 +1,35 @@
+CONVERSION = { "I" => 1,
+  "V" => 5,
+  "X" => 10,
+  "L" => 50,
+  "C" => 100,
+  "D" => 500,
+  "M" => 1000
+}
+
+EXCEPTION = { /IV/ => "IIII",
+  /XL/ => "XXXX",
+  /CD/ => "CCCC"}
+
 def roman_to_integer(roman_string)
   #TODO: translate roman string to integer
+
+  EXCEPTION.each do |old, new|
+    roman_string.sub!(old, new)
+  end
+
+  integer = 0
+
+  roman_string.each_char do |char|
+    integer += CONVERSION[char]
+  end
+  integer
 end
 
 # testing your program
 puts roman_to_integer('IV') == 4 # => true
 puts roman_to_integer('XII') == 12 # => true
 puts roman_to_integer('XIX') == 19 # => true
+
+
 
