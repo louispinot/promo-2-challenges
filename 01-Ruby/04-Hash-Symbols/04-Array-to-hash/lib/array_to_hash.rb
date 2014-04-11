de2c6d02380f29ad52c3fcfd0cf73fed63b76dd2
Louis Pinot <lpvillechenon@gmail.com>
2014-04-11 15:53:09 +0200

    Yeepee!!

--- 01-Ruby/04-Hash-Symbols/04-Array-to-hash/lib/array_to_hash.rb
+++ 01-Ruby/04-Hash-Symbols/04-Array-to-hash/lib/array_to_hash.rb
@@ -1,3 +1,27 @@
+
 def array_to_hash(array)
-  #TODO: implement the method :)
+
+  hash = {}
+  keys_array = []
+  array.each_index {|i| keys_array[i] = i}
+
+  keys_array.each do |index|
+    if block_given?
+      key = yield(index)
+      hash[key] = array[index]
+    else
+      hash[index] = array[index]
+    end
+  end
+
+  hash
 end
+
+
+#yield "kim"
+
+#method(arg) {|nom| nom.capitalize!}
+ puts array_to_hash(%w(aze rty uiop)) { |index| "key#{index + 1}" }
+ puts array_to_hash(%w(aze rty uiop))
+
+
