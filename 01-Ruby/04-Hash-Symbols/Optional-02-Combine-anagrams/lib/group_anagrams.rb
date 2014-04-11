c224654202c3d8cab2eab442c308637e4aec9def
Louis Pinot <lpvillechenon@gmail.com>
2014-04-11 21:30:40 +0200

    nearly finished, only got to implment solution

--- 01-Ruby/04-Hash-Symbols/Optional-02-Combine-anagrams/lib/group_anagrams.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-02-Combine-anagrams/lib/group_anagrams.rb
@@ -1,8 +1,28 @@
-def group_anagrams(words)
-  #TODO: group anagrams
+def group_anagrams(words) # input is an array
+  alphabetized_words ={}
+  words.each do |word|
+
+    alphabetized_words[word] = word.downcase.chars.sort.join
+  end
+  puts alphabetized_words
+
+  boxes_for_words = {}
+  alphabetized_words.each do |original_word, modified_string|
+     boxes_for_words[modified_string]=[]
+  end
+puts
+  puts boxes_for_words
+
+  #alphabetized_words.each do |key, val|
+
+
+
 end
 
 # input:
 p group_anagrams( ['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams', 'scream'] )
 
 # =>  [["cars", "racs", "scar"], ["four"], ["for"], ["potatoes"], ["creams", "scream"]]
+
+arraybis = array.dup
+
