97016dd622453961979dc48f3dd56c1cd9d2d603
Louis Pinot <lpvillechenon@gmail.com>
2014-04-11 17:30:02 +0200

    avec le fichier cette fois...

--- 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
+++ 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
@@ -1,40 +1,51 @@
 # Encoding: utf-8
 
+
+
 def louchebem_translator(word, vowels=%w(a e i o u y))
     unless word.length < 3
-      prepare_end(word)
-      deal_with_start(word)
-      pimp_the_end(word)
+      word = prepare_end(word)
+      word = deal_with_start(word)
+      word = pimp_the_end(word)
     end
+    word
 end
 
 
 def prepare_end (word, vowels=%w(a e i o u y))
-    # last_character = word[-1]
-    # penultimate_character = word[-2]
-
-    unless vowels.include? word[-1]
-      word = word[0..-2] if vowels.include? word[-2]
-      word[-2..-1] = "é" if word[-2..-1] == "er"
+    if word[-2..-1] == "er" || word[-2..-1] == "et"
+      word[-2..-1] = "é"
+    else
+      unless vowels.include?(word[-1]) || word[-1] == "n"  #auquel cas on fait rien
+        word = word[0..-2]
+      end
     end
+    word
 end
-
 
 def deal_with_start (word, vowels=%w(a e i o u y))
 
-     unless vowels.include? word[0]  do # commence par une voyelle
+    ## CONDITIONS
+    first = word[0]
+    second = word[1]
 
-      move_2_letters =  word[0..1] == "qu" || word[0..1] == "gu" || vowels.include? word[1]
+    move0letter = vowels.include? first
+    commence_par_voyelle_puis_consonne = !vowels.include?(first) && vowels.include?(second)
+    commence_pas_par_qu = (word[0..1] != "qu" && word[0..1] != "gu")
+    move1letter = commence_par_voyelle_puis_consonne && commence_pas_par_qu
+    move2letters = (move1letter || move0letter) == false
 
-      if move_2_letters
-          start = word[0..1]
-          word = word[2..-1] + start
-      else
-        start = word[0]
-        word = word[1..-1] + start
-      end
+    if move2letters
+      start = word[0..1]
+      word = word[2..-1] + start
+    elsif move1letter
+      start = word[0]
+      word = word[1..-1] + start
+    else nil        #move0letter
     end
-    word = word.insert(0, "l")
+
+    word = "l" + word
+
 end
 
 
@@ -44,8 +55,10 @@ def pimp_the_end (word)
     word = word.insert(-1, ending)
 end
 
+test = ["filament", "boucher", "comprend", "default", "kiwi", "pasteque", "filet", "filament", "chat", "embrun", "chien", "repas", "boucher", "guerre", "quenouille", "artiste", "spécial", "guerre"]
 
-def louchebemize(sentence)
-  #TODO: implement your louchebem translator
+test.each {|word| p louchebem_translator(word)}
 
-end
+# def louchebemize(sentence)
+#   #TODO: implement your louchebem translator
+# end
