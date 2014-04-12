93e04e3ff2db7bffdbf506e701b721861206504b
Louis Pinot <lpvillechenon@gmail.com>
2014-04-12 15:45:08 +0200

    anagrams over

--- 01-Ruby/04-Hash-Symbols/Optional-02-Combine-anagrams/lib/group_anagrams.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-02-Combine-anagrams/lib/group_anagrams.rb
@@ -1,28 +1,26 @@
 def group_anagrams(words) # input is an array
+
   alphabetized_words ={}
   words.each do |word|
-
     alphabetized_words[word] = word.downcase.chars.sort.join
   end
-  puts alphabetized_words
 
-  boxes_for_words = {}
-  alphabetized_words.each do |original_word, modified_string|
-     boxes_for_words[modified_string]=[]
+  boxes_of_words = {}
+  alphabetized_words.each do |original_word, common_string|
+     boxes_of_words[common_string]=[]
   end
-puts
-  puts boxes_for_words
+  alphabetized_words.each do |original_word, common_string|
+    boxes_of_words[common_string] << original_word
+  end
 
-  #alphabetized_words.each do |key, val|
-
-
+  grouped_anagrams = []
+  boxes_of_words.each do |common_string, array_of_anagrams|
+    grouped_anagrams << array_of_anagrams
+  end
 
 end
 
-# input:
 p group_anagrams( ['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams', 'scream'] )
 
-# =>  [["cars", "racs", "scar"], ["four"], ["for"], ["potatoes"], ["creams", "scream"]]
 
-arraybis = array.dup
 
