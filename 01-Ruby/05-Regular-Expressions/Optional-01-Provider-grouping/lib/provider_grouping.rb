9946bd8ff0321f8f97557e3910a8f1708d6ea761
Louis Pinot <lpvillechenon@gmail.com>
2014-04-14 18:28:13 +0200

    grouping mails

--- 01-Ruby/05-Regular-Expressions/Optional-01-Provider-grouping/lib/provider_grouping.rb
+++ 01-Ruby/05-Regular-Expressions/Optional-01-Provider-grouping/lib/provider_grouping.rb
@@ -1,19 +1,32 @@
-def group_mails( emails ) 
+def group_mails( emails )
   #TODO: group email by provider
+  transit_hash = {}
+  email_hash = {}
+  emails.each do |adress|
+    transit_hash[adress] = adress.split('@')[1].split('.')[0]
+  end
+
+  transit_hash.each_value do|provider|
+    email_hash[provider] = []
+  end
+  transit_hash.each do |adress, provider|
+    email_hash[provider] << adress
+  end
+  email_hash
 end
 
-users_emails = %w( bob@yahoo.fr 
-                   roger57@hotmail.fr 
-                   bigbox@yahoo.fr 
-                   boris@lewagon.org 
-                   monsieur.olivier@gmail.com 
+users_emails = %w( bob@yahoo.fr
+                   roger57@hotmail.fr
+                   bigbox@yahoo.fr
+                   boris@lewagon.org
+                   monsieur.olivier@gmail.com
                    monsieur.mack@gmail.com )
-                   
 
-puts group_mails( users_emails ) 
 
-# => {"yahoo" => ["bob@yahoo.fr", "bigbox@yahoo.fr"], 
-#     "hotmail" => ["roger57@hotmail.fr"], 
-#     "lewagon" => ["boris@lewagon.org"], 
-#     "gmail" => ["monsieur.olivier@gmail.com", 
+puts group_mails( users_emails )
+
+# => {"yahoo" => ["bob@yahoo.fr", "bigbox@yahoo.fr"],
+#     "hotmail" => ["roger57@hotmail.fr"],
+#     "lewagon" => ["boris@lewagon.org"],
+#     "gmail" => ["monsieur.olivier@gmail.com",
 #     "monsieur.mack@gmail.com"]}
