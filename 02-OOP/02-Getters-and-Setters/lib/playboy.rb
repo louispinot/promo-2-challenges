e5caab2f4cf3b28b2078cd88584f81e8d276b5ed
Louis Pinot <lpvillechenon@gmail.com>
2014-04-16 11:39:39 +0200

    solved it

--- 02-OOP/02-Getters-and-Setters/lib/playboy.rb
+++ 02-OOP/02-Getters-and-Setters/lib/playboy.rb
@@ -2,6 +2,10 @@ class Playboy
 
   #TODO: implement necessary getters or setters to run meeting_casanova.rb
 
+  attr_reader :nationality
+  attr_accessor :hair_length
+  attr_reader :conquests
+
   INITIAL_HAIR_LENGTH = 20  # beautiful long playboy hair
 
   def initialize(name, nationality)
