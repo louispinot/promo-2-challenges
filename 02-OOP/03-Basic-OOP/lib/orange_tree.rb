c78cfbc8ff9a960ac84f6f6fb17b90b6954cf0d3
Louis Pinot <lpvillechenon@gmail.com>
2014-04-16 12:45:28 +0200

    my final version

--- 02-OOP/03-Basic-OOP/lib/orange_tree.rb
+++ 02-OOP/03-Basic-OOP/lib/orange_tree.rb
@@ -1,3 +1,39 @@
 class OrangeTree
   # TODO: Implement all the specs defined in the README.md :)
+
+  attr_reader :age, :height, :fruits_on_tree, :dead
+
+  def initialize
+    @age = 0
+    p 'An Orange tree has been planted'
+  end
+
+  def one_year_passes!
+    if @dead == false
+      @age +=1
+      @height +=1 if age <=10
+
+      new_production
+
+      ####################
+      ## test for death ##
+      fate = rand
+      @dead = true if fate < (( @age**2 / 5000 ) - ( @age / 100 ))
+      ######################
+    else
+      p "the villagers think nostalgically about the tree"
+    end
+  end
+
+
+  def new_production
+    @production = 100 if [5, 6, 7, 8, 9].include? @age
+    @production = 200 if [10, 11, 12, 13, 14, 15].include? @age
+    @fruits_on_tree = @production
+  end
+
+  def pick(number_of_oranges)
+    fruits_on_tree -=number_of_oranges if number_of_oranges <= fruits_on_tree
+  end
+
 end
