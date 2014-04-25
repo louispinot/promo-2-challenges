37f5501832fac62bf923ae52a4d52e1629465180
Louis Pinot <lpvillechenon@gmail.com>
2014-04-25 12:37:06 +0200

    reformatting output

--- 03-AR-Database/03-Interacting-with-code/lib/queries.rb
+++ 03-AR-Database/03-Interacting-with-code/lib/queries.rb
@@ -6,27 +6,29 @@ db = SQLite3::Database.new(DATABASE_PATH)
 
 def number_of_rows(db, table_name)
   #TODO: count number of rows in table table_name
-  db.execute("SELECT COUNT(#{table_name}Id) FROM #{table_name}")
+  db.execute("SELECT COUNT(#{table_name}Id) FROM #{table_name}")[0][0]
 end
 
 def sorted_artists(db)
   #TODO: return array of artists' names sorted alphabetically
-  db.execute("SELECT Name FROM Artist ORDER BY Name")
+  array = db.execute("SELECT Name FROM Artist ORDER BY Name")
+  array.map! {|item| item[0]}
 end
 
 def love_tracks(db)
   #TODO: return array of love songs
-  db.execute("SELECT Name FROM Track WHERE Name LIKE '%love%'")
-
+  array = db.execute("SELECT Name FROM Track WHERE Name LIKE '%love%'")
+  array.map! {|item| item[0]}
 end
 
 def long_tracks(db, min_length)
   #TODO: return tracks verifying: duration > min_length (minutes)
   #db.execute("SELECT Name, Milliseconds/60000.0 AS Minutes FROM Track WHERE Milliseconds/60000 > #{min_length}")
-  db.execute("SELECT Name FROM Track WHERE Milliseconds/60000 > #{min_length}")
+  array = db.execute("SELECT Name FROM Track WHERE Milliseconds/60000 > #{min_length}")
+  array.map! {|item| item[0]}
 end
 
-# p number_of_rows(db, 'artist') => OK!
-# sorted_artists(db) => OK!
-# love_tracks(db) => OK!
-# p long_tracks(db, 9) => OK!
+# p number_of_rows(db, 'artist')
+# p sorted_artists(db)
+# p love_tracks(db)
+# p long_tracks(db, 9)
