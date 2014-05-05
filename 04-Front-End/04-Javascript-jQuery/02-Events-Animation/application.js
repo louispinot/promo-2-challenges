52062330baacaa46b0e3421dc67391f75ad0611d
Louis Pinot <lpvillechenon@gmail.com>
2014-05-05 15:55:29 +0200

    Dat be some heavy dynamic shit, nigga!

--- 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
+++ 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
@@ -3,3 +3,32 @@
   * TODO: add your code here!
   *
 */
+
+
+// $('#message').hide()
+
+
+$(document).ready(function(){
+  $('#message').hide()
+
+  displayBox();
+})
+
+function displayBox() {
+  $('#message').slideDown(600, clickToHide());
+}
+
+function clickToHide() {
+  $('#message').click(function(e) {
+    $(this).slideUp(500, toggleTop);
+  });
+}
+
+function toggleTop(){
+ $('#message-wrapper').hover(
+   function(e) { $('#message').slideDown() },
+   function(e) { $('#message').slideUp() }
+  );
+}
+
+
