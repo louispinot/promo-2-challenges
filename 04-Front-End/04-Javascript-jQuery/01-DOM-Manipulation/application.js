49fb7568bb59df81b123a4532df898bb33ed6916
Louis Pinot <lpvillechenon@gmail.com>
2014-05-05 14:48:50 +0200

    DOM manipulation OK

--- 04-Front-End/04-Javascript-jQuery/01-DOM-Manipulation/application.js
+++ 04-Front-End/04-Javascript-jQuery/01-DOM-Manipulation/application.js
@@ -8,48 +8,88 @@
 function run_challenges() {
 
   // Ex 1. Read the content of the email input
-  var email = null;  // TODO: replace null, keep the email variable.
 
+        // <div>
+        //   <label for="email">Email:</label>
+        //   <input type="text" name="email" id="email" value="boris@lewagon.org" autocomplete="off">
+        //   <span id="email-hint">This is not my email!</span>
+        // </div>
+var email = $('input#email').val();
 
 
   // Ex 2. Fill the content of the email input with your email
+
+        // $( "#greatphoto" ).attr( "alt", "Beijing Brush Seller" );
+
+$('input#email').attr('value', 'lpvillechenon@gmail.com');
 
 
 
   // Ex 3. Replace the email hint (next to the input) with 'This is my email now'
   //       The text should be emphasized using a <strong> tag
 
+          // <span id="email-hint">This is not my email!</span>
+          // $('a').text('Text only! HTML will <b>not</b> be understood!');
+
+$('span#email-hint').html('<strong>This is my email now</strong>')
 
   // Ex 4. Add the .blue CSS class to the table header cells (th elements)
 
+        // <table class="table">
+        //   <thead>
+        //     <tr>
+        //       <th>Rang</th>
+        //       <th>Équipe</th>
+        //       <th>Points</th>
+        //       </th>
+        //     </tr>
+        //   </thead>
+
+$('.table th').addClass('blue');  // alternative a .attr('class', 'blue')
 
 
   // Ex 5. Count the number of table body rows there are (team count!)
-  var team_count = 0;  // TODO: replace 0, keep the team_count variable.
-
+  var team_count = $('.table tbody tr').length;  // TODO: replace 0, keep the team_count variable.
 
 
   // Ex 6. Say there is a 15th team added to the table.
   //       Add a row at the bottom, this new team should have zero points.
 
+          // Create a new <p> element with some text
+          // var $newPar = $('<p>').text('Here is a new paragraph!');
+          // // Create a new div and set its HTML content to the new <p>
+          // var $newDiv = $('<div>').html($newPar);
+          // // Add the new div to our document
+          // $('body').append($newDiv);
+
+          // <tr><td>1</td><td>Rugby Club Toulonnais</td><td>77</td></tr>
+
+var $newRow = $('<tr>').html('<td>15</td><td>Le XV de la Choune</td><td>0</td>');
+$('.table tbody').append($newRow);
 
 
   // Ex 7. Write some code to sum all points given to all teams
-  var sum = 0;  // TODO: replace 0 with your sum result, keep the sum variable.
 
-
+var sum = 0;
+var $points = $('.table tbody td:nth-child(3)').each(function(i, el){
+  var score = $(el).text();
+  sum += parseInt(score);
+});
 
   // Ex 8. Change the background color of all table header cells to #DDF4FF
 
+$('.table th').css({background:
+  '#DDF4FF'
+});
 
 
   // Ex 9. Translate the #results list 20px downward (animation would be great ^_^)
 
-
+$('#results').animate({bottom: '-=20px'});
 
   // Ex 10. Remove the "Email:" label from the DOM
 
-
+$('label').remove();
 
   // Checking exercise answers. DO NOT MODIFY BELOW
   assert_equal(email, "boris@lewagon.org");
