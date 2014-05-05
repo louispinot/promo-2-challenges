5d563892fdb6bc0e04da1d14a430560ca5bdf4a4
Louis Pinot <lpvillechenon@gmail.com>
2014-05-05 22:41:13 +0200

    cannot reverse actions so far...

--- 04-Front-End/04-Javascript-jQuery/03-Modal/application.js
+++ 04-Front-End/04-Javascript-jQuery/03-Modal/application.js
@@ -1,5 +1,4 @@
 
-
 $(document).ready(function(){
   modal();
 
@@ -35,8 +34,52 @@ function modal() {
   });
 };
 
-function clear() {
-  $('.modal-background').click(function(e) {
+var cancel = function() {$('img').die( "click" )};
+$('.modal-background').click(cancel);
 
-  });
-};
+
+// $(document).ready(function(){
+//   animate($('img'));
+// });
+
+// function animate(element) {
+//   element.click( modal(element) );
+// };
+
+
+// var modal = function(element) {
+
+//     // this creates a jquery object from the image
+//     var $img = element;
+//     $img.addClass('center').addClass('framed-picture');
+
+//     // this grabs the caption from the alt attribute
+//     var caption = $(this).attr('alt');
+
+//     //this creates a <p> for the caption
+//     var $captionTag = $('<p>').addClass('caption').text(caption);
+
+//     // this drops a dark background on the whole page
+//     var $background = $('<div>').addClass('modal-background');
+//     $('body').append($background);
+
+//     // this creates a white frame
+//     var $frame = $('<div>').addClass('frame').addClass('center');
+
+//     //this adds the caption and picture to the frame
+//     $frame.append($img);
+//     $frame.append($captionTag);
+
+//     //this drops the frame onto the background
+//     $('body').append($frame);
+
+// };
+
+
+
+
+// function clear() {
+//   $('.modal-background').click(function(e) {
+
+//   });
+// };
